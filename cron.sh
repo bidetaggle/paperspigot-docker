b00a327239492c37dca071d0b6a120c455a7351a
elektr.io <bidetaggle@elektr.io>
2020-10-19 13:48:40 +0200

    specify absolute path for cron to work from anywhere in the files system

--- cron.sh
+++ cron.sh
@@ -2,6 +2,7 @@
 
 # Tweaks
 NB_MAX_SNAPSHOTS=2
+ROOT_DIRECTORY=/srv/paperspigot-docker
 
 # Constants
 SNAPSHOTS_DIRECTORY=.snapshots
@@ -14,12 +15,12 @@ VOLUMES=(config worlds plugins data logs)
 DATE=$(date '+%Y-%m-%d-%Hh%Mm%S')
 
 function snapshot_server {
-    docker-compose down
+    cd $ROOT_DIRECTORY && docker-compose down
     echo "Starting server snapshot..."
 
     for volume in ${VOLUMES[*]}; do
         echo "Copying ${volume}..."
-        cp -r ${PROD_DIR_PREFIX}_${volume}/_data $SNAPSHOTS_DIRECTORY/$DATE/${volume}
+        cp -r ${PROD_DIR_PREFIX}_${volume}/_data $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY/$DATE/${volume}
         if [ ! $? -eq 0 ]; then
             echo "Error when copying the volume ${volume}"
             exit 1
@@ -30,12 +31,12 @@ function snapshot_server {
     done
 
     echo "Done."
-    docker-compose up -d
+    cd $ROOT_DIRECTORY && docker-compose up -d
 }
 
 function db_backup {
-    echo "Exporting database from $CONTAINER_NAME to $SNAPSHOTS_DIRECTORY/$DATE/db.sql ..."
-    docker exec -i $CONTAINER_NAME mysqldump --password=$DB_PWD $DB_NAME > $SNAPSHOTS_DIRECTORY/$DATE/db.sql
+    echo "Exporting database from $CONTAINER_NAME to $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY/$DATE/db.sql ..."
+    docker exec -i $CONTAINER_NAME mysqldump --password=$DB_PWD $DB_NAME > $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY/$DATE/db.sql
     
     if [ $? -eq 0 ]; then
         echo "Done."
@@ -47,11 +48,11 @@ function db_backup {
 
 function rolling_backups {
     echo "Starting rolling backup..."
-    NB_SNAPSHOTS=($SNAPSHOTS_DIRECTORY/*)
+    NB_SNAPSHOTS=($ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY/*)
 
     if [ ${#NB_SNAPSHOTS[@]} -gt $NB_MAX_SNAPSHOTS ] ; then
         echo "Deleting the oldest snapshot"
-        rm -rf $SNAPSHOTS_DIRECTORY/$(ls -F $SNAPSHOTS_DIRECTORY | head -n 1)
+        rm -rf $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY/$(ls -F $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY | head -n 1)
     else
         echo "The number of snapshots does not exceed the maximum set (${#NB_SNAPSHOTS[@]}/$NB_MAX_SNAPSHOTS). Rolling nothing."
     fi
@@ -60,13 +61,13 @@ function rolling_backups {
 function on_exit {
     if [ ! $? -eq 0 ]; then
         echo "Something went wrong :( , cleaning up."
-        rm -rf $SNAPSHOTS_DIRECTORY/$(ls -F $SNAPSHOTS_DIRECTORY | head -n 1)
+        rm -rf $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY/$(ls -F $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY | head -n 1)
     else
         echo "Done."
     fi
 }
 
-mkdir -p $SNAPSHOTS_DIRECTORY/$DATE
+mkdir -p $ROOT_DIRECTORY/$SNAPSHOTS_DIRECTORY/$DATE
 trap on_exit EXIT
 
 db_backup
